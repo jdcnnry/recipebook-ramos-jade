ffd45b73f405153f6b201f3b13caae2d5280af66
Jade <jade.ramos@student.ateneo.edu>
2024-03-15 14:59:28 +0800

    Added the Necessary Fields

--- Lab1/recipebook/ledger/admin.py
+++ Lab1/recipebook/ledger/admin.py
@@ -2,18 +2,23 @@ from django.contrib import admin
 
 from .models import Ingredient, Recipe, RecipeIngredient
 
+
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
+
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
 
+
 class RecipeAdmin(admin.ModelAdmin):
-    model = Recipe
     inlines = [RecipeIngredientInline]
+    list_display = ('name', 'author', 'created_on', 'updated_on')
+
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
+
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
--- Lab1/recipebook/ledger/models.py
+++ Lab1/recipebook/ledger/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -7,14 +9,19 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
 
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('ledger:recipe-detail', args=[self.pk])
+
 
 class RecipeIngredient(models.Model):
     qty = models.CharField(max_length=100)
