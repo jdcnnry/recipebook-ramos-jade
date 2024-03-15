33d422567364ac479b2c81e500426e9c5d4833a1
Jade <jade.ramos@student.ateneo.edu>
2024-03-15 14:57:34 +0800

    Fixed URLs

--- Lab1/recipebook/ledger/urls.py
+++ Lab1/recipebook/ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('list/', RecipeListView.as_view(), name='list'),
-    path('<int:pk>', RecipeDetailView.as_view(), name='recipe-detail')
+    path('recipes/list/', RecipeListView.as_view(), name='recipe-list'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail')
 ]
 
 app_name = "ledger"
