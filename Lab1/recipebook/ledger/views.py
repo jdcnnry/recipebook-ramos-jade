2fd9a37abfcffb372fc6cfe0a4a64ee6e98969b7
Jade <jade.ramos@student.ateneo.edu>
2024-03-15 14:58:20 +0800

    Modified Accordingly to Templates

--- Lab1/recipebook/ledger/views.py
+++ Lab1/recipebook/ledger/views.py
@@ -1,25 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Recipe
 
-def recipe_list(request):
-    recipes = Recipe.objects.all()
-    ctx = {"recipes": recipes }
-            
-    return render(request, 'recipe_list.html', ctx)
-
-def recipe_detail(request, pk):
-    recipe = Recipe.objects.get(pk=pk)
-    ctx = {'recipe': recipe}
-
-    return render(request, 'recipe_detail.html', ctx)
 
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipe_list.html'
 
-class RecipeDetailView(DetailView):
+
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
